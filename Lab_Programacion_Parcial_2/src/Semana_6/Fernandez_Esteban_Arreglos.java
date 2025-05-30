4ba016b092559354d02217e8a424ad251664fe39
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-05-30 15:59:28 -0600

    Juego de Ahorcados
    Commit del juego de ahorcado

---
+++ Lab_Programacion_Parcial_2/src/Semana_6/Fernandez_Esteban_Arreglos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_6;
+
+/**
+ *
+ * @author esteb
+ */
+public class Fernandez_Esteban_Arreglos {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
+        
+    }
+}
